a25d5cf5f2f3678bd2e97b0fac42e07829b45ca3
agatakaczmarek <agata.kaczmarek@student.ie.edu>
2018-10-26 14:43:10 +0200

    create linear search function

---
+++ Exercises4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 14:35:46 2018
+
+@author: agatakaczmarek
+"""
+#%%
+def linear(elem,list2):
+        for i in range (len (list2)):
+            if elem==list2[i]:
+                return True
+    
+        return False
